6bb58dc4280e3189991bcb3bb280eb7d2341c867
Vinhuanc <huann.chenn@gmail.com>
2022-09-12 19:48:35 -0500

    code fits the sample outputs. will try to do       display "Your account is overdrawn Only if fee > balance

---
+++ Assign04-Prog03/Assign04-Prog03.cpp
@@ -0,0 +1,59 @@
+//Huan Chen Assignment04-Program03
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main(){
+    double balance, total;
+    double fee = 10;
+    double fee2 = 15;
+    int numCheck;
+    
+    cout << "Enter the following information about your checking account" << "\n";
+    cout << "Beginning balance: $ " ;
+    cin >> balance;
+    cout << "Number of checks written: " ;
+    cin >> numCheck;
+    
+    if(balance < 400){
+        if(numCheck < 20){
+            total = fee2 + (numCheck * 0.10) + balance;
+            cout << "Your account is overdrawn!" << "\n";
+            cout << "The bank fee this month is $ " << setprecision(2) << fixed << total << "\n";
+        }
+        if(numCheck >= 20 && numCheck <= 39){
+            total = fee2 + (numCheck * 0.08) + balance;
+            cout << "Your account is overdrawn!" << "\n";
+            cout << "The bank fee this month is $ " << setprecision(2) << fixed << total << "\n";
+        }
+        if(numCheck >= 40 && numCheck <= 59){
+            total = fee2 + (numCheck * 0.06) + balance;
+            cout << "Your account is overdrawn!" << "\n";
+            cout << "The bank fee this month is $ " << setprecision(2) << fixed << total << "\n";
+        }
+        if(numCheck >= 60){
+            total = fee2 + (numCheck * 0.04) + balance;
+            cout << "Your account is overdrawn!" << "\n";
+            cout << "The bank fee this month is $ " << setprecision(2) << fixed << total << "\n";
+        }
+    }
+    if(balance >= 400){
+        if(numCheck < 20){
+            total = fee + (numCheck * 0.10) ;
+            cout << "The bank fee this month is $ " << setprecision(2) << fixed << total << "\n";
+        }
+        if(numCheck >= 20 && numCheck <= 39){
+            total = fee + (numCheck * 0.08) ;
+            cout << "The bank fee this month is $ " << setprecision(2) << fixed << total << "\n";
+        }
+        if(numCheck >= 40 && numCheck <= 59){
+            total = fee + (numCheck * 0.06) ;
+            cout << "The bank fee this month is $ " << setprecision(2) << fixed << total << "\n";
+        }
+        if(numCheck >= 60){
+            total = fee + (numCheck * 0.04) ;
+            cout << "The bank fee this month is $ " << setprecision(2) << fixed << total << "\n";
+        }
+    }
+}
+
